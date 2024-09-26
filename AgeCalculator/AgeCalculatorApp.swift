804311796dd4269458a94060f249cadb2deb168c
nna144 <llina.tvo@gmail.com>
2024-09-26 21:34:30 +0200

    Initial Commit

---
+++ AgeCalculator/AgeCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgeCalculatorApp.swift
+//  AgeCalculator
+//
+//  Created by Lina Vo on 2024-09-26.
+//
+
+import SwiftUI
+
+@main
+struct AgeCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
