0b54e4a8202c58c803791748abd595ac6b2acf9f
nna144 <llina.tvo@gmail.com>
2024-09-27 15:44:23 +0200

    Add date of birth text field and buttons for calculate/reset

--- AgeCalculator/ContentView.swift
+++ AgeCalculator/ContentView.swift
@@ -8,12 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var dateOfBirth: String = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Age Calculator")
+                .font(.largeTitle)
+                .bold()
+                .padding()
+            
+            Text("Date of birth")
+                .bold()
+            
+            TextField("DD - MM - YYYY", text: $dateOfBirth)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {
+                print("Button tapped!")
+            }) {
+                Text("Calculate")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.orange)
+                    .cornerRadius(10)
+            }
+            
+            Button(action: {
+                print("Button tapped!")
+            }) {
+                Text("Reset")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.orange.opacity(0.5))
+                    .cornerRadius(10)
+            }
+            
         }
         .padding()
     }
